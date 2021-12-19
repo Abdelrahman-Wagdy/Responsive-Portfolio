e05ee9c1c43da22589dd8a4ee43a802a59bfeea1
Abdelrahman-Wagdy <wagdy.abdelrahman@gmail.com>
2021-12-19 18:32:53 +0200

    Introduce myself in homepage

--- js/script.js
+++ js/script.js
@@ -57,3 +57,12 @@ function linkAction(){
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 console.log("Remove menu profile is working!");
+
+// =================== Typewriter Effect =================
+new Typewriter('#Typewriter', {
+  strings: ['AbdelRahman Amr', 'Mechatronics Engineer', 'Web developer'],
+  autoStart: true,
+  loop: true,
+  cursor: "|"
+});
+console.log("Typewritter effect is working!");
