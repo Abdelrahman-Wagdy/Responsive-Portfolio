3f340216739b829546fee96761736ec6dbdbbef4
Abdelrahman-Wagdy <wagdy.abdelrahman@gmail.com>
2021-12-22 11:49:34 +0200

    Updated active link in the menu

--- js/script.js
+++ js/script.js
@@ -100,4 +100,22 @@ function scrollUp() {
     console.log("Scroll up is being called");
 }
 
-window.addEventListener('scroll', scrollUp)
+window.addEventListener('scroll', scrollUp);
+
+// ===================== Scroll section active highlight ================
+const section = document.querySelectorAll('section[id');
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    section.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*='+sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*='+sectionId + ']').classList.remove('active-link')
+        }
+    })
+    console.log('section highlight working!');
+}
+window.addEventListener('scroll', scrollActive)
